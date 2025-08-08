1350790403b47cd4acda5ef31949ac621387bc6c
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2025-08-08 15:40:55 -0500

    Configure asset registry for Metro

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,17 @@
-// metro.config.js
-module.exports = {
-  transformer: {
-    // Puedes habilitar o deshabilitar la configuración TurboModules aquí si es necesario
-    experimentalImportSupport: true,
-    inlineRequires: false,
-  },
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Puedes habilitar o deshabilitar la configuración TurboModules aquí si es necesario
+config.transformer = {
+  ...config.transformer,
+  experimentalImportSupport: true,
+  inlineRequires: false,
 };
+
+config.resolver.assetRegistryPath = require.resolve(
+  'react-native/Libraries/Image/AssetRegistry'
+);
+
+module.exports = config;
+
